e6f0d4636527851f2918b4ee1bd561353c96d94d
Meenakshi Sharma <mee2208sharma@gmail.com>
2020-08-13 22:33:28 +0530

    logged RoomId, UserID, socket connection

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const {v4: uuidV4} = require('uuid')
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.get('/',(req,res)=>{
-    res.redirect('/${uuidV4()}')
+    res.redirect(`/${uuidV4()}`);
 });
 
 app.get('/:room',(req,res)=>{
 res.render('room',{roomId: req.params.room})
 });
+
+io.on('connection',socket=>{
+    socket.on('join-room',(roomId,userId)=>{
+        console.log(roomId,userId);
+    });
+})
 server.listen(3000);
